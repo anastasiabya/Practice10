cc64a7165343acabd32aec4471e5e48825800403
anastasiabya <46025207+anastasiabya@users.noreply.github.com>
2020-05-06 21:57:00 +0300

    Add files via upload

---
+++ Tcp.Client/ClientMainWindow.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+using SomeProject.Library.Client;
+using SomeProject.Library;
+
+namespace SomeProject.TcpClient
+{
+    public partial class ClientMainWindow : Form
+    {
+        public ClientMainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Отправка сообщения на сервер
+        /// </summary>
+        private void OnMsgBtnClick(object sender, EventArgs e)
+        {
+            Client client = new Client();
+            Result res = client.SendMessageToServer(textBox.Text).Result;
+            if(res == Result.OK)
+            {
+                textBox.Text = "";
+                labelRes.Text = "Message was sent succefully!";
+            }
+            else
+            {
+                labelRes.Text = "Cannot send the message to the server.";
+            }
+            timer.Interval = 2000;
+            timer.Start();
+        }
+
+        /// <summary>
+        /// Таймер
+        /// </summary>
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            labelRes.Text = "";
+            timer.Stop();
+        }
+
+        /// <summary>
+        /// Отправка файла на сервер
+        /// </summary>
+        private void sendFileBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Client client = new Client();
+                var res = client.SendFileToServer(fileDialog.FileName);
+                if (res.Result == Result.OK)
+                {
+                    textBox.Text = "";
+                    labelRes.Text = "File was sent succefully!";
+                }
+                else
+                {
+                    labelRes.Text = "Cannot send the file to the server.";
+                }
+
+                timer.Interval = 2000;
+                timer.Start();
+            }
+        }
+    }
+}
